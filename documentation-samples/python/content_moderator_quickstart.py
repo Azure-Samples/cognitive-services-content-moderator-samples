271f754fa0133f77cf4afbb8e46cbd971226de8b
Winona Azure <38537084+wiazur@users.noreply.github.com>
2019-07-17 14:23:40 -0700

    Added pprint()

--- documentation-samples/python/content_moderator_quickstart.py
+++ documentation-samples/python/content_moderator_quickstart.py
@@ -1,6 +1,7 @@
 import os.path
 import time
 import uuid
+from pprint import pprint
 
 from azure.cognitiveservices.vision.contentmoderator import ContentModeratorClient
 from azure.cognitiveservices.vision.contentmoderator.models import (Evaluate, OCR, FoundFaces, # image moderation
@@ -19,7 +20,6 @@ Prerequisites:
     - Install the SDK from the command line: 
         pip install azure-cognitiveservices-vision-contentmoderator
     - Make sure the terms list file is in your working folder: content_moderator_term_list.txt
-    - Create environment variables where indicated (all are before Image Moderation starts).
 
 How to run:
     - From the command line (from working directory): python content_moderator_quickstart.py
@@ -31,14 +31,16 @@ References:
 
 This quickstart contains the following tasks:
     - Image moderation
-    - Text moderation
     - Custom images list
     - Custom terms list
     - Create human images review
 '''
-# Image moderation list
+# Image moderation 
 IMAGE_MODERATION = ['https://moderatorsampleimages.blob.core.windows.net/samples/sample2.jpg',
                     'https://moderatorsampleimages.blob.core.windows.net/samples/sample5.png']
+
+# Text moderation
+TEXT_MODERATION_FILE = 'content_moderator_text_moderation.txt'
 
 # Image lists for the custom image lists
 # Images need to have a minimum of 128 pixels and a maximum file size of 4MB
@@ -55,9 +57,6 @@ IMAGES_TO_MATCH = [ 'https://moderatorsampleimages.blob.core.windows.net/samples
                     'https://moderatorsampleimages.blob.core.windows.net/samples/sample4.png',
                     'https://moderatorsampleimages.blob.core.windows.net/samples/sample5.png',
                     'https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png' ]
-
-# File used for text moderation. Make sure this file is in root folder.
-TEXT_FILE = 'content_moderator_text_moderation.txt'
 
 # Terms for the custom terms list
 TERM_LIST_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)))
@@ -151,13 +150,12 @@ print()
 # Screen the input text: check for profanity, autocorrect text, 
 # check for personally identifying information (PII), and classify text.
 # The parameter 'text_content' expects a File object, which it can call read() on.
-with open(TEXT_FILE, "rb") as text_file:
+with open(TEXT_MODERATION_FILE, "rb") as text_file:
     screen = client.text_moderation.screen_text(language="eng", text_content_type="text/plain", text_content=text_file, autocorrect=True, pii=True, classify=True)
     assert isinstance(screen, Screen)
     # Format and print
     text_mod_results = list(screen.as_dict().items())
-    for result in text_mod_results:
-        print(result)
+    pprint(text_mod_results)
     print()
 '''
 END - TEXT MODERATION
